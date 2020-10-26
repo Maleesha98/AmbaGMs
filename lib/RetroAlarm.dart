10cdeb60387b81e005ec86221f6442339503d414
Chamath Chinthana <62209498+Chamath428@users.noreply.github.com>
2020-10-26 22:43:04 +0530

    Last update

---
+++ lib/RetroAlarm.dart
@@ -0,0 +1,225 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
+//import 'package:testapp3/main.dart';
+import 'main.dart';
+import 'dart:ui';
+
+/*void main() => runApp(MaterialApp(
+  home: RetroAlarm(),
+));*/
+
+class RetroAlarm extends StatefulWidget {
+  @override
+  _RetroAlarmState createState() => _RetroAlarmState();
+}
+
+class _RetroAlarmState extends State<RetroAlarm> {
+
+  int h=09,min=26;
+ // Map<String, int> Alarmdata = {'h':09,'min':26};
+ // int hour,minute;
+  String msg="Let's sleep";
+  _RetroAlarmState(){
+
+    var now = new DateTime.now();
+    String hour = DateFormat('H').format(now);
+    String minute = DateFormat('m').format(now);
+    //this.minute=int.parse(minute);
+    //this.hour=int.parse(hour);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("Assets/wall.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 5.0),
+          child: Container(
+            child: SingleChildScrollView(
+              child: Column(
+                  children: [
+                  SizedBox(height: 50.0),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('MARCUS:',
+                    style: TextStyle(fontFamily: 'Joystick',
+                        fontSize: 18.0,
+                        color: Color.fromRGBO(0, 143, 255, 0.8))
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text("Set your desires",
+                    style: TextStyle(fontFamily: 'Joystick',
+                        fontSize: 18.0,
+                        color: Colors.white)),
+              ),
+              SizedBox(height: 40.0),
+              Wrap(
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 180,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Image(
+                        image: AssetImage('Assets/man.jpg'
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage("Assets/window.png"),
+                          fit: BoxFit.cover,)),
+                    height: 80,
+                    child: FlatButton(
+                        padding: EdgeInsets.fromLTRB(0, -10, 0, -10),
+                        color: Colors.transparent,
+                        onPressed: () {
+                          // Respond to button press
+                        },
+                        child: Text('Set an App',
+                          style: TextStyle(
+                            fontFamily: 'Joystick',
+                            fontSize: 10.0,
+                            color: Colors.yellow,
+                          ),
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        height: 100,
+                        child: FlatButton(
+                          padding: EdgeInsets.fromLTRB(0, -10, 0, -10),
+                          color: Colors.transparent,
+                          onPressed: () {
+                            setState(() {
+                              if (h<12)h++;
+                              else h=00;
+                            });
+                          },
+                          child: Image(
+                            image: AssetImage('Assets/up.png'),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        child: FlatButton(
+                          padding: EdgeInsets.fromLTRB(0, -10, 0, -10),
+                          color: Colors.transparent,
+                          onPressed: () {
+                            setState(() {
+                              if (min<59)min++;
+                              else min=00;
+                            });
+                          },
+                          child: Image(
+                            image: AssetImage('Assets/up.png'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+
+                    child: Row(
+                      children: [
+                        Text(h.toString().padLeft(2, '0'),
+                            style: TextStyle(fontFamily: 'Joystick',
+                                fontSize: 85.0,
+                                color: Colors.white)),
+                        Text(":",
+                            style: TextStyle(fontFamily: 'Joystick',
+                                fontSize: 85.0,
+                                color: Colors.white)),
+                        Text(min.toString().padLeft(2, '0')
+                            ,style: TextStyle(fontFamily: 'Joystick',
+                                fontSize: 85.0,
+                                color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        height: 80,
+                        child: FlatButton(
+                          padding: EdgeInsets.fromLTRB(-5, -5, -5, -5),
+                          color: Colors.transparent,
+                          onPressed: () {
+                            setState(() {
+                              if(h>0)h--;
+                            });
+                          },
+                          child: Image(
+                            image: AssetImage('Assets/down.png'),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 80,
+                        child: FlatButton(
+                          padding: EdgeInsets.all(-2),
+                          color: Colors.transparent,
+                          onPressed: () {
+                            setState(() {
+                              if(min>0)min--;
+                            });
+                          },
+                          child: Image(
+                            image: AssetImage('Assets/down.png'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 100,
+                    child: FlatButton(
+                      padding: EdgeInsets.fromLTRB(-5, -5, -5, -5),
+                      color: Colors.transparent,
+                      onPressed: () {
+                        //Navigator.pushReplacementNamed(context, '/main');
+                        /*Navigator.push(context,
+                            MaterialPageRoute(builder: (context)=>RetroHome())
+                        );*/
+                        //Navigator.pop(context=>RetroHome());
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RetroHome(),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('Assets/Button.png'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+          ]),
+            ),
+        ),
+      ),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
+import 'RetroAlarm.dart';
 
 void main() => runApp(MaterialApp(
-  home: RetroAlarm(),
+  home: RetroHome(),
 ));
 
-class RetroAlarm extends StatelessWidget {
+class RetroHome extends StatefulWidget {
+  @override
+  _RetroHomeState createState() => _RetroHomeState();
+}
+
+class _RetroHomeState extends State<RetroHome> {
+  bool _value=true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("Assets/wall.jpg"),
@@ -31,10 +42,10 @@ class RetroAlarm extends StatelessWidget {
                 ),
                 Align(
                   alignment: Alignment.center,
-                  child: Text("You have 8 hours to sleep",
-                    style: TextStyle(fontFamily: 'Joystick',
-                        fontSize: 18.0,
-                        color: Colors.white)),
+                  child: Text("I will wake you on time",
+                      style: TextStyle(fontFamily: 'Joystick',
+                          fontSize: 18.0,
+                          color: Colors.white)),
                 ),
                 SizedBox(height: 40.0),
                 Container(
@@ -48,99 +59,83 @@ class RetroAlarm extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      height: 100,
-                      child: FlatButton(
-                        padding: EdgeInsets.fromLTRB(0, -10, 0, -10),
-                        color: Colors.transparent,
-                        onPressed: () {
-                          // Respond to button press
-                        },
-                        child: Image(
-                          image: AssetImage('Assets/up.png'),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 100,
-                      child: FlatButton(
-                        padding: EdgeInsets.fromLTRB(0, -10, 0, -10),
-                        color: Colors.transparent,
-                        onPressed: () {
-                          // Respond to button press
-                        },
-                        child: Image(
-                          image: AssetImage('Assets/up.png'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
                 Container(
+
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 3),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage("Assets/window.png"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
 
                   child: Row(
                     children: [
+                      /*LiteRollingSwitch(
+                        textSize: 10,
+                        value: true,
+                        textOn: "On",
+                        textOff: "Off",
+                        colorOn: Colors.deepOrange,
+                        colorOff: Colors.lightBlue,
+                        iconOn: Icons.alarm_on,
+                        iconOff: Icons.alarm_off,
+                      ),*/
                       Text("09",
                           style: TextStyle(fontFamily: 'Joystick',
-                              fontSize: 85.0,
+                              fontSize: 60.0,
                               color: Colors.white)),
                       Text(":",
                           style: TextStyle(fontFamily: 'Joystick',
-                              fontSize: 85.0,
+                              fontSize: 60.0,
                               color: Colors.white)),
                       Text("15",style: TextStyle(fontFamily: 'Joystick',
-                          fontSize: 85.0,
+                          fontSize: 60.0,
                           color: Colors.white)),
+
+                      SizedBox(width: 40),
+                      Column(
+
+                        children: [
+                          Switch(value: _value,
+                            onChanged: (val){
+                              setState(() {
+                                _value=val;
+                              });
+                            },
+                          ),
+
+                          SizedBox(height: 30),
+                          Stack(
+                            children: [
+                              Container(
+                                alignment : Alignment(-1, -1),
+                                child :Text("AM",style: TextStyle(fontFamily: 'Joystick',
+                                    fontSize: 32.0,
+                                    color: Colors.white)),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      height: 100,
-                      child: FlatButton(
-                        padding: EdgeInsets.fromLTRB(-5, -5, -5, -5),
-                        color: Colors.transparent,
-                        onPressed: () {
-                          // Respond to button press
-                        },
-                        child: Image(
-                          image: AssetImage('Assets/down.png'),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 100,
-                      child: FlatButton(
-                        padding: EdgeInsets.all(-2),
-                        color: Colors.transparent,
-                        onPressed: () {
-                          // Respond to button press
-                        },
-                        child: Image(
-                          image: AssetImage('Assets/down.png'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  height: 100,
-                  child: FlatButton(
-                    padding: EdgeInsets.fromLTRB(-5, -5, -5, -5),
-                    color: Colors.transparent,
-                    onPressed: () {
-                      // Respond to button press
-                    },
+
+
+                FlatButton(onPressed: (){
+
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context)=>RetroAlarm())
+                  );
+                },
                     child: Image(
-                      image: AssetImage('Assets/Button.png'),
-                    ),
-                  ),
-                ),
+                      image: AssetImage('Assets/Add Button.png'),
+                    ))
               ],
             ),
           ),
